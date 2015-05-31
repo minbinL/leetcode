b8780aab3a7e86832712a9a9afefdd78d79bef3e
socosting <lmbfantasy@gmail.com>
2015-06-01 00:11:24 +0800

    Update dungeon-game.cpp
    Correct the answer.

--- C++/dungeon-game.cpp
+++ C++/dungeon-game.cpp
@@ -4,10 +4,13 @@ public:
         int m = dungeon.size();
 	if (m == 0) return -1;
 	int n = dungeon[0].size();
-        vector
+        vector <vector<int> > dp (m, vector<int> (n, 1));
 	for (int i = n-1; i >= 0; i--)
-            for (int j = m-1; j >=0; j++) {
-		if (i == n-1 && j == m-1) 
+            for (int j = m-1; j >=0; j--) {
+		if (i == n-1 && j == m-1) dp[i][j] = max(0-dungeon[i][j], 0);
+		else if (i == n-1) dp[i][j] = max(dp[i][j+1]-dungeon[i][j], 0);
+		else if (j == m-1) dp[i][j] = max(dp[i+1][j]-dungeon[i][j], 0);
+		else dp[i][j] = max(min(dp[i+1][j]-dungeon[i][j], dp[i][j+1]-dungeon[i][j]), 0);
 	}
         return dungeon[0][0] + 1;
     }
